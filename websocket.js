618d0697aadee4d406e0547375af4ce6ab641f53
Clarence <mr.lin.clarence@gmail.com>
2021-08-24 12:02:49 +0800

    Add websocket user uuid support

--- websocket.js
+++ websocket.js
@@ -1,16 +1,18 @@
 const websocket = require('./websockets/websocket')
 const users = require('./websockets/users')
+const { uuidv4 } = require('./modules/utils')
 
 function upgrade(request, socket, head) {
     const pathname = request.url;
+    const uuid = uuidv4()
 
     if (pathname === '/websocket') {
         websocket.handleUpgrade(request, socket, head, function done(ws) {
-            websocket.emit('connection', ws, request);
+            websocket.emit('connection', ws, request, uuid);
         });
     } else if (pathname === '/users') {
         users.handleUpgrade(request, socket, head, function done(ws) {
-            users.emit('connection', ws, request);
+            users.emit('connection', ws, request, uuid);
         });
     } else {
         socket.destroy();
--- websockets/websocket.js
+++ websockets/websocket.js
@@ -1,7 +1,7 @@
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ noServer: true });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, req, uuid) {
     const id = setInterval(function () {
         ws.send(JSON.stringify(process.memoryUsage()), function () {
             //
@@ -9,10 +9,10 @@ wss.on('connection', function connection(ws) {
             //
         });
     }, 100);
-    console.log('started client interval');
+    console.log(`started client interval uuid: ${uuid}`);
 
     ws.on('close', function () {
-        console.log('stopping client interval');
+        console.log(`stopping client interval uuid: ${uuid}`);
         clearInterval(id);
     });
 });
